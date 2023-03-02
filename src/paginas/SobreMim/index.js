78223b5d8683bc3e7a1468269f1553d4e9cd7323
Eli Junior <eli.junior3@aluno.unip.br>
2023-03-02 13:48:32 -0300

    Update Sobre Mim

--- src/paginas/SobreMim/index.js
+++ src/paginas/SobreMim/index.js
@@ -2,7 +2,7 @@ import PostModelo from 'componentes/PostModelo'
 import React from 'react'
 import fotoCapa from 'assets/sobre_mim_capa.png'
 import styles from './SobreMim.module.css'
-import fotoSobreMim from 'assets/sobre_mim_foto.png'
+import fotoSobreMim from 'assets/meu_avatar.png'
 
 export default function SobreMim() {
   return (
@@ -19,22 +19,16 @@ export default function SobreMim() {
         className={styles.fotoSobreMim}
       />
       <p className={styles.paragrafo}>
-        Oi, tudo bem? Eu sou instrutor de Front-end na Alura e estou feliz de te ver por aqui.
+        Oi, tudo bem? Eu sou um Desenvolvedor Web e estou feliz de te ver por aqui.
       </p>
       <p className={styles.paragrafo}>
-        Minha história com programação começou no Instituto Federal do Piauí (IFPI), quando fiz o ensino médio integrado ao curso de Informática. Eu aprendi lógica de programação e o básico de várias linguagens, como PHP, Python, Java, mas sem aprofundar muito em cada uma delas. Eu gostava muito de estudar programação, mas na época não fazia ideia de que trabalharia com isso hoje.
+        Minha história com programação começou no Colégio Fernão Dias, quando fiz o ensino médio integrado ao curso de Informática. Eu aprendi lógica de programação e o básico de várias linguagens, como HTML, Delphi, Visual Basic e C mas sem aprofundar muito em cada uma delas. Eu gostava muito de estudar programação, mas na época não fazia ideia de que trabalharia com isso hoje.
       </p>
       <p className={styles.paragrafo}>
-        No ensino superior, escolhi cursar Engenharia Elétrica na Universidade Federal do Piauí (UFPI). Lá eu consegui entrar no PET (Programa de Educação Tutorial), um grupo acadêmico que contribui de diversas formas para o curso. Lá eu tive a oportunidade de estudar desenvolvimento web para ser um dos mantenedores do site deles. 
+        Atualmente tenho conhecimentos em Python para desenvolvimento do Back-End e no Front-End estou desenvolvendo em React JS. Também tenho conhecimentos em HTML, CSS, JavaScript, e alguns bancos de dados como MySQL e PostgreSQL.
       </p>
       <p className={styles.paragrafo}>
-        Com isso tive minha experiência de dev mais próxima da realidade, com prazos de implementação para o site, e aprendi muito enquanto codificava.
-      </p>
-      <p className={styles.paragrafo}>
-        E foi enquanto eu estava no PET que decidi procurar um estágio em desenvolvimento web. Eu encontrei uma vaga na Alura para o Scuba Team e percebi que ela aliava duas paixões minhas: programação e educação. Para minha felicidade, fui escolhido para fazer parte do time.
-      </p>
-      <p className={styles.paragrafo}>
-        Desde então, tem sido aprenas aprendizados atrás de aprendizados. A Alura é uma escola não só para seus alunos e alunas, mas também para os colaboradores e colaboradoras. Hoje sou muito feliz de ter a oportunidade de trazer esses conteúdos ricos e encantadores para você. Espero que aprenda bastante!
+        Este site foi desenvolvido com o intuito de desenvolver conhecimentos novos em React JS e também para compartilhar conhecimentos que eu já tenho. Espero que você goste!
       </p>
 
     </PostModelo>
